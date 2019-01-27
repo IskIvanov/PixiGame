66dc211e54c65db23b60a635a533cd06172142e1
Iskren Ivanov <iskren93@gmail.com>
2019-01-27 21:04:39 +0200

    laser added

--- app.js
+++ app.js
@@ -5,18 +5,12 @@ renderer.view.style.position = 'absolute';
 renderer.view.style.top = '50%';
 renderer.view.style.left = '50%';
 renderer.view.style.transform = 'translate3d( -50%, -50%, 0 )';
+renderer.view.style.color = '0x990000';
 
 const stage = new PIXI.Container();
+stage.interactive = true;
 
-const texture = PIXI.Texture.fromImage('./assets/sniperSlice.png');  
-const carrotTex = PIXI.Texture.fromImage('assets/rocket.png');
-const sniper = new PIXI.Sprite(texture);
-sniper.anchor.x = 0.5;  
-sniper.anchor.y = 0.5;
-
-sniper.position.x = 200;  
-sniper.position.y = 150;
-
+//Drowing background for the canvas
 const background = new PIXI.Graphics();  
 background.beginFill(0x11111);  
 background.drawRect(0,0,800,600);  
@@ -24,14 +18,31 @@ background.endFill();
 
 stage.addChild(background);
 
-stage.addChild(sniper);
 
-stage.interactive = true;
+const texture = PIXI.Texture.fromImage('./assets/sniperSlice.png');  
+const rocket = PIXI.Texture.fromImage('assets/rocket.png');
+const sniper = new PIXI.Sprite(texture);
+
+sniper.anchor.x = 0.5;  
+sniper.anchor.y = 0.5;
+
+//sniper Starting position
+sniper.position.x = 200;  
+sniper.position.y = 150;
+
+//Drawing laser for the sniper
+var laser = new PIXI.Graphics();
+laser.lineStyle(1.2, 0xba0000);
+laser.moveTo(0,-20);
+laser.lineTo(300, 100);
+stage.addChild(laser);
+
+stage.addChild(sniper);
 
 stage.on("mousedown", function(e){  
   shoot(sniper.rotation, {
-    x: sniper.position.x+Math.cos(sniper.rotation)*20,
-    y: sniper.position.y+Math.sin(sniper.rotation)*20
+    x: sniper.position.x,
+    y: sniper.position.y
   });
 })
 
@@ -41,10 +52,6 @@ stage.on("rightclick", function(e){
 
 let bullets = [];  
 const bulletSpeed = 10;
-
-const getMousePosition = () => {
-      renderer.plugins.interaction.mouse.global;
-}
 
 (function setup() {
      animate();  
@@ -56,16 +63,17 @@ function moveToPoint() {
 }
 
 function shoot(rotation, startPosition){  
-  var bullet = new PIXI.Sprite(carrotTex);
+  var bullet = new PIXI.Sprite(rocket);
+  
   bullet.position.x = startPosition.x;
   bullet.position.y = startPosition.y;
+
   bullet.rotation = rotation;
   stage.addChild(bullet);
   bullets.push(bullet);
 }
 
 function rotateToPoint(mx, my, px, py){  
-  var self = this;
   var dist_Y = my - py;
   var dist_X = mx - px;
   var angle = Math.atan2(dist_Y,dist_X);
@@ -75,7 +83,15 @@ function rotateToPoint(mx, my, px, py){
 
 function animate() {  
 requestAnimationFrame(animate);
+
 sniper.rotation = rotateToPoint(renderer.plugins.interaction.mouse.global.x, renderer.plugins.interaction.mouse.global.y, sniper.position.x, sniper.position.y);
+
+laser.rotation = rotateToPoint(renderer.plugins.interaction.mouse.global.x, renderer.plugins.interaction.mouse.global.y, laser.position.x, laser.position.y);
+
+laser.rotation+=100.15;
+
+laser.position.x = sniper.position.x;
+laser.position.y = sniper.position.y;
 
 for (var b=bullets.length-1; b>=0; b--){
     bullets[b].position.x += Math.cos(bullets[b].rotation)*bulletSpeed;
